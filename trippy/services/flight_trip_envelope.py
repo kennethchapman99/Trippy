a56eae1b5cc9cfafb0869c3fb55c7d41480ea9bb
Kenneth Chapman <kchapman@lh9yd4f04y.desire2learn.d2l>
2026-05-12 10:32:04 -0400

    Format flight trip envelope imports

--- trippy/services/flight_trip_envelope.py
+++ trippy/services/flight_trip_envelope.py
@@ -11,7 +11,12 @@ import re
 from datetime import date, datetime
 from typing import Literal
 
-from trippy.models.shortlists import FlightOption, ResearchShortlistState, ShortlistCategory, ShortlistRowStatus
+from trippy.models.shortlists import (
+    FlightOption,
+    ResearchShortlistState,
+    ShortlistCategory,
+    ShortlistRowStatus,
+)
 
 FlightSelectionKind = Literal["outbound", "return"]
 
